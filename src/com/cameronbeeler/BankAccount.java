672fc81842689513a382a7c065b6a1dfe846ca42
Cameron Beeler <cameron.beeler@gmail.com>
2018-11-28 14:48:17 -0800

    threadsafe using the reentrant lock/unlock methods

--- src/com/cameronbeeler/BankAccount.java
+++ src/com/cameronbeeler/BankAccount.java
@@ -1,29 +1,50 @@
 package com.cameronbeeler;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public
 class BankAccount
 {
-    private double balance;
-    private String accountNumber;
+    private       double balance;
+    private       String accountNumber;
+    private final Lock   lock;
 
     public BankAccount(String accountNumber, double initialBalance)
     {
         this.balance = initialBalance;
         this.accountNumber = accountNumber;
+        lock = new ReentrantLock();
     }
 
-    public synchronized void deposit(double amount)
+    public void deposit(double amount)
     {
+        lock.lock();
+        try
+        {
             System.out.println("new deposit amount $" + amount);
             this.balance += amount;
             System.out.println("new balance amount $" + this.balance);
+        }
+        finally
+        {
+            lock.unlock();
+        }
     }
 
     public synchronized void withdraw(double amount)
     {
+        lock.lock();
+        try
+        {
             System.out.println("new withdrawal amount $" + amount);
             this.balance -= amount;
             System.out.println("new balance amount $" + this.balance);
+        }
+        finally
+        {
+            lock.unlock();
+        }
     }
 
     public
